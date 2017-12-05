cf90413009c2902beea24d77616656fc8c46a960
Himanshu Kumar <himanshu4746@gmail.com>
2017-12-05 10:25:15 +0530

    Update 7.pl

--- 7.pl
+++ 7.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
-use CGI':standard';
-print "Refresh:1\n";
-print "Content-type:text/html\n\n";
-($s,$m,$h)=localtime(time);
-print "<center>$h:$m:$s</center>";
+
+print "content-type:text/html\n\n";
+print "<html><head><title>Digital Clock</title>";
+print "<META HTTP-EQUIV=refresh content=0;";
+print "URL=http://localhost/cgi-bin/7.pl>";
+($s,$m,$h) = localtime(time);
+print "</head><body bgcolor=lightgreen><center><b>";
+print "DIGITAL CLOCK IS SHOWN BELOW :<br><br>";
+print"$h:$m:$s</b></center></body></html>";
